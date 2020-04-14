2eb99eb65a82fb26fe2986f7879dac72283c987d
David <davidgilm123@gmail.com>
2020-04-14 12:32:55 +0200

    Declaración de atributos, constructores y getters/etters

--- src/main/java/com/mycompany/t6cdavidgil/Persona.java
+++ src/main/java/com/mycompany/t6cdavidgil/Persona.java
@@ -10,5 +10,48 @@ package com.mycompany.t6cdavidgil;
  * @author david
  */
 public class Persona {
+    //Declaramos atributos
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    
+    // Constructores
+    public Persona() {
+        this.nombre = "David";
+        this.apellidos = "Gil Manzano";
+        this.edad = 20;
+    }
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    
+    //Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
     
 }
